bb5a60582e15540bb8a80369c16c3627d34f8312
DuckWichTrust <68964814+AmirhosseinAghighi@users.noreply.github.com>
2021-08-12 16:19:26 +0430

    Update Sql.sql

--- Duck_Cad/Sql.sql
+++ Duck_Cad/Sql.sql
@@ -12,17 +12,8 @@ CREATE TABLE IF NOT EXISTS `duckcad_data` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE users ADD WantedLevel TEXT
-ALTER TABLE users ALTER WantedLevel SET DEFAULT 'standard'
+ALTER TABLE `owned_vehicles` ADD `WantedLevel` TEXT NULL DEFAULT 'standard';
+ALTER TABLE `owned_vehicles` ADD `Profile_Pic` TEXT NULL DEFAULT 'https://media.discordapp.net/attachments/813604209462214676/858319794900959232/unknown.png?width=1201&height=676';
 
-ALTER TABLE users ADD Profile_Pic TEXT
-ALTER TABLE users ALTER Profile_Pic SET DEFAULT 'https://media.discordapp.net/attachments/813604209462214676/858319794900959232/unknown.png?width=1201&height=676'
-
-
-
-
-ALTER TABLE owned_vehicles ADD WantedLevel TEXT
-ALTER TABLE owned_vehicles ALTER WantedLevel SET DEFAULT 'standard'
-
-ALTER TABLE owned_vehicles ADD Profile_Pic TEXT
-ALTER TABLE owned_vehicles ALTER Profile_Pic SET DEFAULT 'https://media.discordapp.net/attachments/813604209462214676/858319794900959232/unknown.png?width=1201&height=676'
+ALTER TABLE `users ` ADD `WantedLevel` TEXT NULL DEFAULT 'standard';
+ALTER TABLE `users ` ADD `Profile_Pic` TEXT NULL DEFAULT 'https://media.discordapp.net/attachments/813604209462214676/858319794900959232/unknown.png?width=1201&height=676';
